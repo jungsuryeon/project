bf4b81147ef8025c21f6467a5abde18c6369965a
jungsuryeon <srj327@naver.com>
2025-03-21 05:17:01 +0000

    Initial commit

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ import { owner, repo} from './config.js';
     const contributors = await getContributors(owner, repo);
     const scores = calculateScores(contributors);
 
-    console.log('기여도 점수:');
+    alert.log('기여도 점수:');
     scores.forEach(user => {
-      console.log(`${user.login}: ${user.score}점`);
+      alert.log(`${user.login}: ${user.score}점`);
     });
   } catch (err) {
-    console.error('오류 발생:', err);
+    alert.error('오류 발생:', err);
   }
 })();
